4856fd12ad10a2cf521e562a544ffa567c822b0b
Maulana Hanif Al Faqih Rojichan <maulanahanif33@gmail.com>
2023-08-06 11:24:28 +0700

    fix when edit text in detail deposit fragment is empty, modify home page fragment, and change icon date

--- app/src/main/java/id/allana/titipbarangku/util/SnackbarExt.kt
+++ app/src/main/java/id/allana/titipbarangku/util/SnackbarExt.kt
@@ -2,7 +2,13 @@ package id.allana.titipbarangku.util
 
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
+import id.allana.titipbarangku.R
 
-fun View.snackbar(msg: String) {
-    Snackbar.make(this, msg, Snackbar.LENGTH_SHORT).show()
+fun View.snackbar(msg: String, view: View) {
+    val snackbar = Snackbar.make(this, msg, Snackbar.LENGTH_SHORT)
+    snackbar.setAction(context.getString(R.string.close)) {
+        snackbar.dismiss()
+    }
+    snackbar.setAnchorView(view)
+    snackbar.show()
 }
