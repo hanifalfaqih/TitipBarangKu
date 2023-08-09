6ed3bf6a08fb92dfc3ed248c4207fa1be5f996cc
Maulana Hanif Al Faqih Rojichan <maulanahanif33@gmail.com>
2023-08-09 08:47:12 +0700

    update code format rupiah

--- app/src/main/java/id/allana/titipbarangku/util/FormatRupiah.kt
+++ app/src/main/java/id/allana/titipbarangku/util/FormatRupiah.kt
@@ -1,11 +1,12 @@
 package id.allana.titipbarangku.util
 
 import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 import java.util.Locale
 
 fun formatRupiah(text: String): String {
     val cleanString = text.replace("[Rp,.\\s]".toRegex(), "")
-    val convertToDecimal = cleanString.toDouble() / 100
-    val formatter = DecimalFormat.getCurrencyInstance(Locale("id", "ID"))
-    return formatter.format(convertToDecimal)
+    val convertToLong = cleanString.toLong()
+    val formatter = DecimalFormat("Rp#,###", DecimalFormatSymbols(Locale("id", "ID")))//DecimalFormat.getCurrencyInstance(Locale("id", "ID"))
+    return formatter.format(convertToLong)
 }
